98e7731a3d82a027c455ba810be256a527fb8d1d
Yulia Zinovieva <Kiss-ka1-833@mail.ru>
2022-09-25 09:08:17 +0300

    'Correction WordsChecker'

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,29 +1,19 @@
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class WordsChecker {
     protected String text;
-    HashMap<String, Integer> map = new HashMap<>();
+    protected Set <String> words = new HashSet<>();  //сохраним слова в поле чтобы потом не переразбивать, создаем множество,т.к. оно предполагает поиск элементов
 
     public WordsChecker(String text) {
         this.text = text;
+        String [] partsWord = text.split("\\P{IsAlphabetic}+");  //сразу разбивка на слова+заполняем множество
+        for (String word : partsWord) {  //переберем все слова
+           words.add(word);              //каждое слово вставляем в коллекцию Сет
+        }
     }
 
-    public boolean hasWord() {
-        String[] item = text.split("\\P{IsAlphabetic}+");
-        for (String t : item) {
-            if (map.containsKey(t)) {
-                map.put(t, map.get(t) + 1);
-            } else {
-                map.put(t, 1);
-            }
-        }
-        Set<String> keysPack = map.keySet();
-        for (String key : keysPack) {
-            if (map.get(key) > 1) {
-                System.out.println(key + " - " + map.get(key));
-            }
-        }
-        return true;
-    }
+    public boolean hasWord(String wordQuery) {
+        return words.contains(wordQuery);            //ответ тем же самым содержит множество слово или нет
+    }                                                //т.е. метод ответит тем же самым что и ответит коллекция
 }
